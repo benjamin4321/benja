f6e080e192b0cda99330e1d49e03f4d11ca94247
benjamin4321 <114407770+benjamin4321@users.noreply.github.com>
2022-10-13 13:59:04 +0200

    Update teststreamlit2.py

--- teststreamlit2.py
+++ teststreamlit2.py
@@ -181,7 +181,7 @@ print_model = model.summary()
 
 explanotory_data = pd.DataFrame({"ChargeTime": np.arange(0, 10)})
 
-#mdl_con_vs_char = ols("ConnectedTime ~ ChargeTime", data=df_laadpaal).fit()
+mdl_con_vs_char = OLS("ConnectedTime ~ ChargeTime", data=df_laadpaal).fit()
 
 explanotory_data = pd.DataFrame({"ChargeTime": np.arange(0, 10)})
 
